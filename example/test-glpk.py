7c5a1bebaff89ff631586da2b9ecc66988d2ede9
Roland Averkamp <roland.averkamp.0@gmail.com>
2023-07-18 21:40:52 +0200

    black formatting

--- example/test-glpk.py
+++ example/test-glpk.py
@@ -1,20 +1,17 @@
 from avdiagram import glpk
 
+
 def a():
     p = glpk.Problem()
-    v1 = p.addvar('a',0,10)
-    v2 = p.addvar('b',0,10)
-    p.addConstraint([(v1,1),(v2,3)],'LE',4)
-    p.addConstraint([(v1,12),(v2,3)],'LE',13)
-    p.addConstraint([(v1,0.8)],'GE',0.07)
-    p.setObjective('MAX',[(v1,1),(v2,1)])
+    v1 = p.addvar("a", 0, 10)
+    v2 = p.addvar("b", 0, 10)
+    p.addConstraint([(v1, 1), (v2, 3)], "LE", 4)
+    p.addConstraint([(v1, 12), (v2, 3)], "LE", 13)
+    p.addConstraint([(v1, 0.8)], "GE", 0.07)
+    p.setObjective("MAX", [(v1, 1), (v2, 1)])
     p.run()
     for k in p.vardict:
-        print(k,"=", p.vardict[k].value)
+        print(k, "=", p.vardict[k].value)
+
 
 a()
-    
-    
-    
-    
-        
--- test/test_glpk.py
+++ test/test_glpk.py
@@ -2,22 +2,23 @@
 import unittest
 from avdiagram import glpk
 
-class Test_simple(unittest.TestCase):
 
+class Test_simple(unittest.TestCase):
     def test_success(self):
         p = glpk.Problem()
-        v = p.addvar("a",0, 2)
-        p.addConstraint([(v,1)],"GE",1)
-        p.setObjective("MIN",[(v,1)])
+        v = p.addvar("a", 0, 2)
+        p.addConstraint([(v, 1)], "GE", 1)
+        p.setObjective("MIN", [(v, 1)])
         self.assertTrue(p.run())
-        self.assertEqual(v.value,1)
+        self.assertEqual(v.value, 1)
 
     def test_fail(self):
         p = glpk.Problem()
-        v = p.addvar("a",-1, 0)
-        p.addConstraint([(v,1)],"GE",1)
-        p.setObjective("MIN",[(v,1)])
+        v = p.addvar("a", -1, 0)
+        p.addConstraint([(v, 1)], "GE", 1)
+        p.setObjective("MIN", [(v, 1)])
         self.assertFalse(p.run())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
